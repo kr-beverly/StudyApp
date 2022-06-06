1607864305599a3a7818075a1fdc96edb2963d13
Bae juhyeon <wjdqhqhgh724148@naver.com>
2022-06-06 21:43:32 +0900

    button change 0506-9pm

--- app/src/main/java/com/example/myapplication_study/Signup.java
+++ app/src/main/java/com/example/myapplication_study/Signup.java
@@ -8,9 +8,20 @@ import android.view.View;
 import android.widget.Button;
 
 public class Signup extends AppCompatActivity {
+    Button complete_save_user_sign;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+
+        complete_save_user_sign = findViewById(R.id.complete_save_user_sign);
+        complete_save_user_sign.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(Signup.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
