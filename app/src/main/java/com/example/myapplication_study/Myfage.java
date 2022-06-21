8620c0ae2ea41e26b2b14414a1ed4b729aa3cc1c
Bae juhyeon <wjdqhqhgh724148@naver.com>
2022-06-21 21:29:14 +0900

    0621-chage-Myfage

--- app/src/main/java/com/example/myapplication_study/Myfage.java
+++ app/src/main/java/com/example/myapplication_study/Myfage.java
@@ -10,6 +10,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class Myfage extends Fragment {
     private View view;
@@ -18,6 +24,27 @@ public class Myfage extends Fragment {
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_myfage, container, false);
+
+        CheckBox ch_login= view.findViewById(R.id.ch_login);
+        Button myfage_loginbtn = view.findViewById(R.id.myfage_loginbtn);
+        myfage_loginbtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(), loginfrom.class);
+                startActivity(intent);
+            }
+        });
+
+        ch_login.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                if(ch_login.isChecked()) {
+                    myfage_loginbtn.setVisibility(View.VISIBLE);
+                } else {
+                    myfage_loginbtn.setVisibility(View.VISIBLE);
+                }
+            }
+        });
 
         return view;
     }
