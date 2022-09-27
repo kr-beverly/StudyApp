7ddd31e7d144a9cf63761263baf116d0d9c24a0b
Bae juhyeon <wjdqhqhgh724148@naver.com>
2022-09-27 11:15:15 +0900

    게시판 목록 리스트

--- app/src/main/java/com/example/myapplication_study/SignNotice.java
+++ app/src/main/java/com/example/myapplication_study/SignNotice.java
@@ -6,19 +6,45 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ListView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
 public class SignNotice extends Fragment {
     private Button sign_notice_write_btn;
     private View view;
+
+    ListView listView;
+    SignNoticeAdapter signNoticeAdapter;
+    public static ArrayList<SignNoticeList> signNoticeListArrayList = new ArrayList<>();
+    String url = "http://capstudyapp.dothome.co.kr/BoardList.php";
+    SignNoticeList signNoticeList;
 
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         view = inflater.inflate(R.layout.activity_signnotice, container, false);
+
+        listView = view.findViewById(R.id.sign_listView);
+        signNoticeAdapter = new SignNoticeAdapter(getContext(), signNoticeListArrayList);
+        listView.setAdapter(signNoticeAdapter);
+        retrieveData();
 
         //글쓰기 창으로 넘어감
         sign_notice_write_btn = view.findViewById(R.id.sign_notice_write_btn);
@@ -31,5 +57,46 @@ public class SignNotice extends Fragment {
         });
 
         return view;
+    }
+
+    public void retrieveData(){
+        StringRequest request = new StringRequest(Request.Method.POST, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        signNoticeListArrayList.clear();
+                        try{
+
+                            JSONObject jsonObject = new JSONObject(response);
+                            String success = jsonObject.getString("success");
+                            JSONArray jsonArray = jsonObject.getJSONArray("data");
+
+                            if(success.equals("1")){
+                                for(int i=0;i<jsonArray.length();i++) {
+
+                                    JSONObject object = jsonArray.getJSONObject(i);
+
+                                    String writ_num = object.getString("writ_num");
+                                    String writ_title = object.getString("writ_title");
+
+                                    signNoticeList = new SignNoticeList(writ_num, writ_title);
+                                    signNoticeListArrayList.add(signNoticeList);
+                                    signNoticeAdapter.notifyDataSetChanged();
+                                }
+                            }
+                        }
+                        catch (JSONException e){
+                            e.printStackTrace();
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(getActivity(), error.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        RequestQueue requestQueue = Volley.newRequestQueue(getActivity());
+        requestQueue.add(request);
     }
 }
---
+++ app/src/main/java/com/example/myapplication_study/SignNoticeAdapter.java
@@ -0,0 +1,41 @@
+package com.example.myapplication_study;
+
+import android.content.Context;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import java.util.List;
+
+public class SignNoticeAdapter extends ArrayAdapter<SignNoticeList> {
+
+    Context context;
+    List<SignNoticeList> arraysignNoticeList;
+
+    public SignNoticeAdapter(@NonNull Context context, List<SignNoticeList> arraysignNoticeList) {
+        super(context, R.layout.activity_sign_notice_list, arraysignNoticeList);
+
+        this.context = context;
+        this.arraysignNoticeList = arraysignNoticeList;
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_sign_notice_list, null, true);
+
+        TextView tvnum = view.findViewById(R.id.notice_num);
+        TextView tvtitle = view.findViewById(R.id.notice_title);
+
+        tvnum.setText(arraysignNoticeList.get(position).getWrit_num());
+        tvtitle.setText(arraysignNoticeList.get(position).getWrit_title());
+
+        return view;
+    }
+}
